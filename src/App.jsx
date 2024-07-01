c81e516d6b7450229db256d14add16a57d44a8bc
Herrera-Pablo <pablogastonherrerat@gmail.com>
2024-07-01 16:09:47 -0300

    se agrego info a features y cardsection1

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import AboutUs from './components/AboutUs';
-import CarouselSection from './components/CarouselSection';
+// import CarouselSection from './components/CarouselSection';
 import CardsSection1 from './components/CardsSection1';
 import CardsSection2 from './components/CardsSection2';
 import Contact from './components/Contact';
 import FeaturesSection from './components/FeaturesSection';
-import TestimonialsSection from './components/TestimonialsSection';
-import CTASection from './components/CTASection';
+// import TestimonialsSection from './components/TestimonialsSection';
+// import CTASection from './components/CTASection';
 import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
@@ -47,7 +47,7 @@ const HomePage = () => {
             <section id="features">
                 <FeaturesSection />
             </section>
-            <section id="cta">
+          {/*   <section id="cta">
                 <CTASection />
             </section>
             <section id="carousel">
@@ -55,16 +55,16 @@ const HomePage = () => {
             </section>
             <section id="testimonials">
                 <TestimonialsSection />
-            </section>
+            </section>*/}
             <section id="cards1">
                 <CardsSection1 />
             </section>
-            <section id="cards2">
+           {/*  <section id="cards2">
                 <CardsSection2 />
             </section>
             <section id="about">
                 <AboutUs />
-            </section>
+            </section> */}
             <section id="contact">
                 <Contact />
             </section>
