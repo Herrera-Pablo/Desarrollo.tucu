d77aa2814db067216cce65a3d9e99da801c8d5d0
Herrera-Pablo <pablogastonherrerat@gmail.com>
2024-07-03 16:35:34 -0300

    creación error404

---
+++ src/pages/error404.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/error404.css'; // Asegúrate de tener estilos adecuados
+
+const Error404 = () => {
+    return (
+        <div className="error404-container">
+            <h1>404</h1>
+            <h2>Página no encontrada</h2>
+            <p>Lo sentimos, pero la página que buscas no existe.</p>
+            <Link to="/" className="go-home-button">Ir a la página principal</Link>
+        </div>
+    );
+};
+
+export default Error404;
